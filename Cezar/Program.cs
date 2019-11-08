1ec54ecc9d20fad6b28dbf4bb010d9483c97e194
Toga <rodionphilippov@gmail.com>
2019-11-08 13:25:09 +0400

    created

---
+++ Cezar/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+
+namespace Cezar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ-.?!0123456789";
+
+            Console.WriteLine(alph.Length);
+
+            Console.WriteLine("Enter text for encryption:");
+            string inputText = Console.ReadLine();
+
+            Console.WriteLine("Result of encryption:");
+            Console.WriteLine(TextEncryption(inputText, alph));
+
+            Console.WriteLine("Result of decryption:");
+            Console.WriteLine(TextDecryption(TextEncryption(inputText, alph), alph));
+
+            Console.ReadKey();
+        }
+
+        static string TextEncryption(string inputText, string alph)
+        {
+            int n = 4, k = 5, m = alph.Length;
+
+            string result = "";
+
+            for (int i = 0; i < inputText.Length; i++)
+                for (int j = 0; j < alph.Length; j++)
+                    if (inputText[i].Equals(alph[j]))
+                    {
+                        result += alph[(j + n) % m];
+                        break;
+                    }
+
+            return result;
+        }
+
+        static string TextDecryption(string inputText, string alph)
+        {
+            int n = 4, k = 5, m = alph.Length;
+
+            string result = "";
+
+            for (int i = 0; i < inputText.Length; i++)
+                for (int j = 0; j < alph.Length; j++)
+                    if (inputText[i] == alph[j])
+                    {
+                            result += alph[(j - n) % m];
+                            break;
+                    }
+
+                    return result;
+        }
+    }
+}
